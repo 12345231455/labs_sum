fd5cd7530f22eacdbad4eaa408c49384fb504372
12345231455 <147683836+12345231455@users.noreply.github.com>
2024-07-10 18:30:12 +0300

    Add files via upload

---
+++ генератор тестов.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	ofstream out("input.txt");
+	int n, q;
+	n = rand() % 100000;
+	q = rand() % 100000;
+	out << n << '\n';
+	vector<long long> a(n);
+	vector<long long> b(q);
+	for (int i = 0; i < n; i++)
+	{
+		a[i] = rand() % 10000000;
+		out << a[i] << '\n';
+	}
+	out << q << '\n';
+	for (int i = 0; i < q; i++)
+	{
+		b[i] = rand() % 10000000;
+		out << b[i] << '\n';
+	}
+}
