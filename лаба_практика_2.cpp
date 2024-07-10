ee66a20536ecef514470edb31a8d32c757cc4d66
12345231455 <147683836+12345231455@users.noreply.github.com>
2024-07-10 10:32:19 +0300

    Update лаба_практика_2.cpp

--- лаба_практика_2.cpp
+++ лаба_практика_2.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <iomanip>
 #include <vector>
 #include <algorithm>
@@ -8,242 +8,242 @@ using namespace std;
 
 void Pref_1d()
 {
-	// инициализация
+        // инициализация
 
-	int n;
-	cin >> n;
-	vector<int> a(n + 1);
-	vector<long long> Pr(n + 1);
+        int n;
+        cin >> n;
+        vector<int> a(n + 1);
+        vector<long long> Pr(n + 1);
 
-	// база
+        // база
 
-	Pr[0] = 0;
+        Pr[0] = 0;
 
-	// счет
+        // счет
 
-	for (int i = 1; i <= n; i++)
-	{
-		cin >> a[i];
-		Pr[i] = Pr[i - 1] + a[i];
-	}
+        for (int i = 1; i <= n; i++)
+        {
+                cin >> a[i];
+                Pr[i] = Pr[i - 1] + a[i];
+        }
 
-	// ответ
+        // ответ
 
-	for (int i = 0; i <= n; i++)
-	{
-		cout << Pr[i] << ' ';
-	}
+        for (int i = 0; i <= n; i++)
+        {
+                cout << Pr[i] << ' ';
+        }
 }
 
 void Pref_2d()
 {
 
-	// инициализация
+        // инициализация
 
-	int n, m;
-	cin >> n >> m;
-	vector<vector<int>> a(n, vector<int> (m));
-	vector<vector<int>> Pr(n, vector<int>(m));
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < m; j++)
-		{
-			cin >> a[i][j];
-		}
-	}
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> a(n, vector<int> (m));
+        vector<vector<int>> Pr(n, vector<int>(m));
+        for (int i = 0; i < n; i++)
+        {
+                for (int j = 0; j < m; j++)
+                {
+                        cin >> a[i][j];
+                }
+        }
 
-	// база
+        // база
 
-	Pr[0][0] = a[0][0];
+        Pr[0][0] = a[0][0];
 
-	for (int i = 1; i < n; i++)
-	{
-		Pr[i][0] = Pr[i - 1][0] + a[i][0];
-	}
-	for (int i = 1; i < m; i++)
-	{
-		Pr[0][i] = Pr[0][i - 1] + a[0][i];
-	}
+        for (int i = 1; i < n; i++)
+        {
+                Pr[i][0] = Pr[i - 1][0] + a[i][0];
+        }
+        for (int i = 1; i < m; i++)
+        {
+                Pr[0][i] = Pr[0][i - 1] + a[0][i];
+        }
 
-	// счет
+        // счет
 
-	for (int i = 1; i < n; i++)
-	{
-		for (int j = 1; j < m; j++)
-		{
-			Pr[i][j] = max(Pr[i - 1][j], Pr[i][j - 1]) + a[i][j];
-		}
-	}
+        for (int i = 1; i < n; i++)
+        {
+                for (int j = 1; j < m; j++)
+                {
+                        Pr[i][j] = max(Pr[i - 1][j], Pr[i][j - 1]) + a[i][j];
+                }
+        }
 
-	// ответ
+        // ответ
 
-	cout << Pr[n - 1][m - 1];
+        cout << Pr[n - 1][m - 1];
 }
 
 void NVP_school()
 {
 
-	// инициализация
+        // инициализация
 
-	int n;
-	cin >> n;
-	vector<int> a(n);
-	for (int i = 0; i < n; i++)
-	{
-		cin >> a[i];
-	}
-	vector<int> Dp_len(n + 1, 1);
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+        {
+                cin >> a[i];
+        }
+        vector<int> Dp_len(n + 1, 1);
 
-	// счет
+        // счет
 
-	for (int i = 1; i < n; i++)
-	{
-		for (int j = 0; j < i; j++)
-		{
-			if (a[j] < a[i])
-			{
-				Dp_len[i] = max(Dp_len[i], Dp_len[j] + 1);
-			}
-		}
-	}
-	int mx = 0;
-	for (int i = 0; i <= n; i++)
-	{
-		if (Dp_len[i] > mx)
-		{
-			mx = Dp_len[i];
-		}
-	}
-	cout << mx;
+        for (int i = 1; i < n; i++)
+        {
+                for (int j = 0; j < i; j++)
+                {
+                        if (a[j] < a[i])
+                        {
+                                Dp_len[i] = max(Dp_len[i], Dp_len[j] + 1);
+                        }
+                }
+        }
+        int mx = 0;
+        for (int i = 0; i <= n; i++)
+        {
+                if (Dp_len[i] > mx)
+                {
+                        mx = Dp_len[i];
+                }
+        }
+        cout << mx;
 }
 
 int Bin_search(vector<int>& best, int x)
 {
-	int l = 0;
-	int r = best.size();
-	while (l < r)
-	{
-		int mid = l + (r - l) / 2;
-		if (best[mid] <= x)
-		{
-			l = mid + 1;
-		}
-		else
-		{
-			r = mid;
-		}
-	}
-	return l;
+        int l = 0;
+        int r = best.size();
+        while (l < r)
+        {
+                int mid = l + (r - l) / 2;
+                if (best[mid] <= x)
+                {
+                        l = mid + 1;
+                }
+                else
+                {
+                        r = mid;
+                }
+        }
+        return l;
 }
 
 void NVP_stud()
 {
 
-	// инициализация
+        // инициализация
 
-	int n;
-	cin >> n;
-	vector<int> a(n);
-	for (int i = 0; i < n; i++)
-	{
-		cin >> a[i];
-	}
-	vector<int> Dp_len(n + 2, 1e9);
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+        {
+                cin >> a[i];
+        }
+        vector<int> Dp_len(n + 2, 1e9);
 
-	// база
+        // база
 
-	Dp_len[0] = 0;
+        Dp_len[0] = 0;
 
-	// счет
+        // счет
 
-	for (int i = 0; i < n; i++)
-	{
-		int j = Bin_search(Dp_len, a[i]);
-		if (Dp_len[j - 1] < a[i] && a[i] < Dp_len[j])
-		{
-			Dp_len[j] = a[i];
-		}
-	}
+        for (int i = 0; i < n; i++)
+        {
+                int j = Bin_search(Dp_len, a[i]);
+                if (Dp_len[j - 1] < a[i] && a[i] < Dp_len[j])
+                {
+                        Dp_len[j] = a[i];
+                }
+        }
 
-	// ответ
+        // ответ
 
-	int mx = 0;
-	int ind = 0;
-	for (int i = 0; i <= n; i++)
-	{
-		if (Dp_len[i] > mx && Dp_len[i] != 1e9)
-		{
-			mx = Dp_len[i];
-			ind = i;
-		}
-	}
-	cout << ind;
+        int mx = 0;
+        int ind = 0;
+        for (int i = 0; i <= n; i++)
+        {
+                if (Dp_len[i] > mx && Dp_len[i] != 1e9)
+                {
+                        mx = Dp_len[i];
+                        ind = i;
+                }
+        }
+        cout << ind;
 }
 
 void backpack()
 {
 
-	// инициализация
+        // инициализация
 
-	int n;
-	cin >> n;
-	vector<int> weight(n);
-	vector<int> cost(n);
-	for (int i = 0; i < n; i++)
-	{
-		cin >> weight[i];
-		cin >> cost[i];
-	}
-	int s;
-	cin >> s;
-	vector<vector<int>> Dp_backpack(n + 1, vector<int>(s + 1));
+        int n;
+        cin >> n;
+        vector<int> weight(n + 1);
+        vector<int> cost(n + 1);
+        for (int i = 1; i <= n; i++)
+        {
+                cin >> weight[i];
+                cin >> cost[i];
+        }
+        int s;
+        cin >> s;
+        vector<vector<int>> Dp_backpack(n + 1, vector<int>(s + 1));
 
-	// база
+        // база
 
-	Dp_backpack[0][0] = 0;
-	for (int i = 1; i <= s; i++)
-	{
-		Dp_backpack[0][i] = -1;
-	}
+        Dp_backpack[0][0] = 0;
+        for (int i = 1; i <= s; i++)
+        {
+                Dp_backpack[0][i] = -1;
+        }
 
-	// счет
+        // счет
 
-	for (int i = 1; i <= n; i++)
-	{
-		for (int j = 0; j <= s; j++)
-		{
-			Dp_backpack[i][j] = Dp_backpack[i - 1][j];
-		}
-		for (int j = 0; j <= s; j++)
-		{
-			if (j - weight[i] >= 0)
-			{
-				if (Dp_backpack[i - 1][j - weight[i]] != -1)
-				{
-					Dp_backpack[i][j] = max(Dp_backpack[i][j], Dp_backpack[i - 1][j - weight[i]] + cost[i]);
-				}
-			}
-		}
-	}
+        for (int i = 1; i <= n; i++)
+        {
+                for (int j = 0; j <= s; j++)
+                {
+                        Dp_backpack[i][j] = Dp_backpack[i - 1][j];
+                }
+                for (int j = 0; j <= s; j++)
+                {
+                        if (j - weight[i] >= 0)
+                        {
+                                if (Dp_backpack[i - 1][j - weight[i]] != -1)
+                                {
+                                        Dp_backpack[i][j] = max(Dp_backpack[i][j], Dp_backpack[i - 1][j - weight[i]] + cost[i]);
+                                }
+                        }
+                }
+        }
 
-	// ответ
+        // ответ
 
-	int mx = 0;
-	for (int i = 0; i <= s; i++)
-	{
-		if (mx < Dp_backpack[n][i])
-		{
-			mx = Dp_backpack[n][i];
-		}
-	}
-	cout << mx;
+        int mx = 0;
+        for (int i = 0; i <= s; i++)
+        {
+                if (mx < Dp_backpack[n][i])
+                {
+                        mx = Dp_backpack[n][i];
+                }
+        }
+        cout << mx;
 }
 
 int main()
 {
-	//Pref_1d();
-	//Pref_2d();
-	//NVP_school();
-	//NVP_stud();
-	backpack();
+        //Pref_1d();
+        //Pref_2d();
+        //NVP_school();
+        //NVP_stud();
+        //backpack();
 }
