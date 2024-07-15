60be58ed43b5f1b9d480adb607ca651f74dfd27c
12345231455 <147683836+12345231455@users.noreply.github.com>
2024-07-15 21:12:40 +0300

    Add files via upload

--- лаба_практика_1.cpp
+++ лаба_практика_1.cpp
@@ -1,150 +1,132 @@
 ﻿#include <iostream>
+#include <set>
 #include <vector>
 #include <queue>
-#include <set>
 #include <stack>
 
 using namespace std;
 
-void BFS_comp (vector<vector<int>>& G, vector<int>& Mark, int st)
+void BFS_comp(vector<vector<int>>& G, vector<int>& Mark, int start)
 {
-    queue<int> que;
-    que.push(st);
-    Mark[st] = 1;
-    int tmp;
-    while (que.size() > 0)
-    {
-        tmp = que.front();
-        que.pop();
-        for (int i = 0; i < G[tmp].size(); i++)
-        {
-            if (Mark[G[tmp][i]] == 0)
-            {
-                Mark[G[tmp][i]] = 1;
-                que.push(G[tmp][i]);
-            }
-        }
-    }
+	queue<int> q;
+	q.push(start);
+	Mark[start] = 1;
+	int tmp;
+	while (q.size() > 0)
+	{
+		tmp = q.front();
+		q.pop();
+		for (int i = 0; i < G[tmp].size(); i++)
+		{
+			if (Mark[G[tmp][i]] == 0)
+			{
+				Mark[G[tmp][i]] = 1;
+				q.push(G[tmp][i]);
+			}
+		}
+	}
 }
 
-void BFS_comp_con()
+void BFS_comp_connect()
 {
-
-    // инициализация и считывание данных
-
-    int n, m;
-    cout << "n, m: ";
-    cin >> n >> m;
-    vector<vector<int>> G(n + 1);
-    vector<int>Mark(n + 1);
-    int a, b;
-    for (int i = 0; i < m; i++)
-    {
-        cin >> a >> b;
-        G[a].push_back(b);
-        G[b].push_back(a);
-    }
-    int count = 0; 
-
-    // решение
-
-    for (int i = 1; i <= n; i++)
-    {
-        if (Mark[i] == 0)
-        {
-            count++;
-            BFS_comp(G, Mark, i);
-        }
-    }
-    cout << count << '\n';
+	int n, m;
+	cin >> n >> m;
+	vector<vector<int>> G(n + 1);
+	vector<int>Mark(n + 1);
+	int a, b;
+	int cnt = 0;
+	for (int i = 0; i < m; i++)
+	{
+		cin >> a >> b;
+		G[a].push_back(b);
+		G[b].push_back(a);
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		if (Mark[i] == 0)
+		{
+			cnt++;
+			BFS_comp(G, Mark, i);
+		}
+	}
+	cout << '\n' << cnt << '\n';
 }
 
-void BFS_short_path()
+void BFS_short()
 {
-
-    // инициализация и считывание данных
-
-    int n, m;
-    cin >> n >> m;
-    vector<vector<int>> G(n + 1);
-    vector<int>Len(n + 1);
-    int a, b;
-    for (int i = 0; i < m; i++)
-    {
-        cin >> a >> b;
-        G[a].push_back(b);
-        G[b].push_back(a);
-    }
-    int st, fin;
-    cin >> st >> fin;
-    queue<int> que;
-    que.push(st);
-    vector<int>Mark(n + 1);
-    Mark[st] = 1;
-    vector<int> Path(n + 1);
-    Path[st] = st;
-
-    // решение
-
-    while (que.size() > 0)
-    {
-        int tmp = que.front();
-        que.pop();
-        for (int i = 0; i < G[tmp].size(); i++)
-        {
-            if (Mark[G[tmp][i]] == 0)
-            {
-                Mark[G[tmp][i]] = 1;
-                Path[G[tmp][i]] = tmp;
-                Len[G[tmp][i]] = 1 + Len[tmp];
-                que.push(G[tmp][i]);
-            }
-        }
-    }
-    if (Mark[fin] == 0)
-    {
-        cout << -1;
-    }
-    else
-    {
-        cout << Len[fin] << '\n';
-        vector<int> path;
-        for (int i = fin; i != st; i = Path[i]) 
-        {
-            path.push_back(i);
-        }
-        path.push_back(st);
-        reverse(path.begin(), path.end());
-        for (int i = 0; i < path.size(); i++)
-        {
-            cout << path[i] << ' ';
-        }
-    }
+	int start, finish, n, m;
+	cin >> start >> finish;
+	cin >> n >> m;
+	queue<int>q;
+	q.push(start);
+	vector<vector<int>> G(n + 1);
+	vector<int>Mark(n + 1);
+	Mark[start] = 1;
+	vector<int> Put(n + 1);
+	Put[start] = start;
+	vector<int>Len(n + 1);
+	int a, b;
+	for (int i = 0; i < m; i++)
+	{
+		cin >> a >> b;
+		G[a].push_back(b);
+		G[b].push_back(a);
+	}
+	while (q.size() > 0)
+	{
+		int tmp = q.front();
+		q.pop();
+		for (int i = 0; i < G[tmp].size(); i++)
+		{
+			if (Mark[G[tmp][i]] == 0)
+			{
+				Mark[G[tmp][i]] = 1;
+				Put[G[tmp][i]] = tmp;
+				Len[G[tmp][i]] = 1 + Len[tmp];
+				q.push(G[tmp][i]);
+			}
+		}
+	}
+	if (Mark[finish] == 0)
+	{
+		cout << -1;
+	}
+	else
+	{
+		cout << Len[finish] << endl;
+		vector<int> path;
+		for (int i = finish; i != start; i = Put[i]) {
+			path.push_back(i);
+		}
+		path.push_back(start);
+		reverse(path.begin(), path.end());
+		for (int i = 0; i < path.size(); i++)
+		{
+			cout << path[i] << " ";
+		}
+	}
 }
 
-void DFS_comp_ (int x, vector<vector<int>>& G, vector<int>& Mark)
+void dfs(int x, vector<vector<int>>& G, vector<int>& Mark)
 {
 	Mark[x] = 1;
 	for (int i = 0; i < G[x].size(); i++)
 	{
 		if (Mark[G[x][i]] == 0)
 		{
-			DFS_comp_(G[x][i], G, Mark);
+			dfs(G[x][i], G, Mark);
 		}
 	}
 }
 
-void DFS_comp_con()
+void dfs_comp_connect()
 {
-
-	// инициализация и чтение данных
-
 	int n, m;
 	cin >> n >> m;
 	vector<vector<int>> G(n + 1);
 	vector <int> Mark(n + 1);
 	int a, b;
-	int count = 0;
+	int res = 0;
 	for (int i = 0; i < m; i++)
 	{
 		cin >> a >> b;
@@ -152,21 +134,18 @@ void DFS_comp_con()
 		G[b].push_back(a);
 
 	}
-
-	// решение
-
 	for (int i = 1; i <= n; i++)
 	{
 		if (Mark[i] == 0)
 		{
-			count++;
-			DFS_comp_(i, G, Mark);
+			res++;
+			dfs(i, G, Mark);
 		}
 	}
-	cout << count << '\n';
+	cout << res << '\n';
 }
 
-void DFS_cycle_exist_ (int x, int y, vector<vector<int>>& G, vector<int> Mark, bool& flag)
+void dfs_cycle(int x, int y, vector<vector<int>>& G, vector<int>& Mark, bool& flag)
 {
 	//y - предок
 	Mark[x] = 1;
@@ -174,7 +153,7 @@ void DFS_cycle_exist_ (int x, int y, vector<vector<int>>& G, vector<int> Mark, b
 	{
 		if (Mark[G[x][i]] == 0)
 		{
-			DFS_cycle_exist_(G[x][i], x, G, Mark, flag);
+			dfs_cycle(G[x][i], x, G, Mark, flag);
 		}
 		else
 		{
@@ -186,13 +165,9 @@ void DFS_cycle_exist_ (int x, int y, vector<vector<int>>& G, vector<int> Mark, b
 	}
 }
 
-void DFS_cycle_exist()
+void dfs_cycle_exist()
 {
-
-	// инициализация и чтение данных
-
 	int n, m;
-	cout << "n, m: ";
 	cin >> n >> m;
 	vector<vector<int>> G(n + 1);
 	vector<int>Mark(n + 1);
@@ -204,14 +179,11 @@ void DFS_cycle_exist()
 		G[b].push_back(a);
 	}
 	bool flag = false;
-
-	// решение
-
 	for (int i = 1; i <= n && flag == false; i++)
 	{
 		if (Mark[i] == 0)
 		{
-			DFS_cycle_exist_(i, -1, G, Mark, flag);
+			dfs_cycle(i, -1, G, Mark, flag);
 		}
 	}
 	if (flag)
@@ -224,7 +196,7 @@ void DFS_cycle_exist()
 	}
 }
 
-void DFS_cycle_ (int x, int y, vector<vector<int>>& G, vector<int> Mark, bool& flag, stack<int>& st)
+void dfs_cycle_recov(int x, int y, vector<vector<int>>& G, vector<int> Mark, bool& flag, stack<int>& st)
 {
 	Mark[x] = 1;
 	st.push(x);
@@ -234,7 +206,7 @@ void DFS_cycle_ (int x, int y, vector<vector<int>>& G, vector<int> Mark, bool& f
 		{
 			if (Mark[G[x][i]] == 0)
 			{
-				DFS_cycle_(G[x][i], x, G, Mark, flag, st);
+				dfs_cycle_recov(G[x][i], x, G, Mark, flag, st);
 			}
 			else
 			{
@@ -250,10 +222,9 @@ void DFS_cycle_ (int x, int y, vector<vector<int>>& G, vector<int> Mark, bool& f
 	}
 }
 
-void DFS_cycle()
+void dfs_cycle_recovery()
 {
 	int n, m;
-	cout << "n, m: ";
 	cin >> n >> m;
 	vector<vector<int>> G(n + 1);
 	vector<int>Mark(n + 1);
@@ -270,32 +241,138 @@ void DFS_cycle()
 	{
 		if (Mark[i] == 0)
 		{
-			DFS_cycle_(i, -1, G, Mark, flag, st);
+			dfs_cycle_recov(i, -1, G, Mark, flag, st);
 		}
 	}
-	if (!st.empty())
+	if (flag == true)
 	{
-		int a1 = st.top();
+		int tmp = st.top();
+		cout << st.top();
 		st.pop();
-		cout << a1 << " ";
-		while (st.top() != a1)
+		while (st.top() != tmp)
 		{
-			cout << st.top() << " ";
+			cout << st.top() << ' ';
 			st.pop();
 		}
-		cout << st.top();
+		cout << tmp << '\n';
 	}
 	else
 	{
-		cout << "no cycle" << '\n';
+		cout << "NO CYCLE" << '\n';
 	}
+}
+
+void len_1(vector<vector<pair<int, int>>>& G, vector<int>& used, vector<int>& Len, int start, int finish, int n)
+{
+	Len[start] = 0;
+	int tmp;
+	for (int i = 0; i < n; i++) // цикл кол-во релакс
+	{
+		tmp = -1;
+		int length = 1e9;
+		for (int j = 1; j <= n; j++)
+		{
+			if (used[j] == 0)
+			{
+				if (length > Len[j])
+				{
+					length = Len[j];
+					tmp = j;
+				}
+			}
+		}
+		if (tmp != -1)
+		{
+			used[tmp] = 1;
+			for (int j = 0; j < G[tmp].size(); j++)
+			{
+				Len[G[tmp][j].second] = min(Len[G[tmp][j].second], Len[tmp] + G[tmp][j].first);
+			}
+		}
+	}
+	if (Len[finish] != 1e9)
+	{
+		cout << Len[finish] << '\n';
+	}
+	else
+	{
+		cout << -1 << '\n';
+	}
+}
+
+void deykstra_school()
+{
+	int n, m, start, finish;
+	cin >> n >> m;
+	cin >> start >> finish;
+	vector<vector<pair<int, int>>> G(n + 1);
+	vector<int> used(n + 1), Len(n + 1, 1e9);
+	for (int i = 0; i < m; i++)
+	{
+		int a, b, weight;
+		cin >> a >> b >> weight;
+		G[a].emplace_back(weight, b);
+		G[b].emplace_back(weight, a);
+	}
+	len_1(G, used, Len, start, finish, n);
+}
+
+void Len_2(vector<vector<pair<int, int>>>& G, vector<int>& Mark, vector<int>& Len, int start, int finish, int n)
+{
+	set<pair<int, int>> st;
+	pair <int, int> tmp1, tmp2;
+	tmp1.first = 0;
+	tmp1.second = start;
+	Mark[start] = 1;
+	st.insert(tmp1);
+	while (st.size())
+	{
+		tmp1 = *st.begin();
+		st.erase(st.begin());
+		if (Len[tmp1.second] > tmp1.first)
+		{
+			Len[tmp1.second] = tmp1.first;
+			Mark[tmp1.second] = 1;
+			for (int i = 0; i < G[tmp1.second].size(); i++)
+			{
+				tmp2.second = G[tmp1.second][i].second;
+				tmp2.first = tmp1.first + G[tmp1.second][i].first;
+				st.insert(tmp2);
+			}
+		}
+	}
+	if (Mark[finish] == 1)
+	{
+		cout << Len[finish];
+	}
+}
+
+void deykstra_stud()
+{
+	int n, m, start, finish;
+	cout << "n, m: ";
+	cin >> n >> m;
+	cout << endl << "start, finish: ";
+	cin >> start >> finish;
+	vector<vector<pair<int, int>>> G(n + 1);
+	vector<int> Mark(n + 1), Len(n + 1, 1e9);
+	for (int i = 0; i < m; i++)
+	{
+		int a, b, weight;
+		cin >> a >> b >> weight;
+		G[a].emplace_back(weight, b);
+		G[b].emplace_back(weight, a);
+	}
+	Len_2(G, Mark, Len, start, finish, n);
 }
 
 int main()
 {
-    //BFS_short_path();
-	//BFS_comp_con();
-	//DFS_comp_con();
-	//DFS_cycle_exist();
-	//DFS_cycle();
+	//BFS_comp_connect();
+	//BFS_short();
+	//dfs_comp_connect();
+	//dfs_cycle_exist();
+	//dfs_cycle_recovery();
+	//deykstra_school();
+	//deykstra_stud();
 }
